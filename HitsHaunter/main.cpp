a4deb492d743d4b72c01cf89af56ffb7506cb07a
Ben Wilson <benjamin.james.wilson@cern.ch>
2025-04-08 11:08:07 +0100

    finish RDF code

--- HitsHaunter/main.cpp
+++ HitsHaunter/main.cpp
@@ -6,6 +6,12 @@
 #include "RootReader.h"
 #include "RootWriter.h"
 #include "Event.h"
+#include "Hit.h"
+#include "StripModule.h"
+
+#include <ROOT/RDataFrame.hxx>
+#include <ROOT/RDFHelpers.hxx>
+
 
 // Function to parse command-line arguments
 std::unordered_map<std::string, std::string> parseArgs(int argc, char* argv[]) {
@@ -23,6 +29,34 @@ std::unordered_map<std::string, std::string> parseArgs(int argc, char* argv[]) {
     }
     return args;
 }
+
+
+std::vector<Hit> processOneEvent(const std::vector<double>& x, const std::vector<double>& y, const std::vector<double>& z, const std::vector<double>& charge, const std::vector<double>& energy, const std::vector<int>& pdgc, const std::vector<int>& layer, const std::vector<SCTModule>& modules) {
+    std::vector<Hit> new_hits{};
+    std::vector<Hit> hits = {};
+
+    for (unsigned int i{0}; i < x.size(); i++)
+    {   
+        Hit hit;
+        hit.x = x.at(i);
+        hit.y = y.at(i);
+        hit.z = z.at(i);
+        hit.pdgc = pdgc.at(i);
+        hit.energy = energy.at(i);
+        hit.charge = charge.at(i);
+        hit.layer = layer.at(i);
+
+        hits.push_back(hit);
+    }
+
+    for (const auto& module: modules)
+    {   
+        std::vector<Hit> digit_hits = module.digitizeHits(hits);
+        new_hits.insert(new_hits.end(), digit_hits.begin(), digit_hits.end());
+    }
+    return new_hits;
+}
+
 
 int main(int argc, char* argv[]) {
     
@@ -53,8 +87,12 @@ int main(int argc, char* argv[]) {
     }
 
     // Create the RootReader and RootWriter objects
-    RootReader reader{inputFile.c_str(), "Hits"};
-    RootWriter writer{outputFile.c_str(), "Hits"};
+    ROOT::EnableImplicitMT();
+    ROOT::RDataFrame df{"Hits", inputFile.c_str()};
+    ROOT::RDF::Experimental::AddProgressBar(df);
+
+    // auto df = df1.Range(100);
+    
     
     //* Initialise SCT Modules
     double module_start_pos = 550 + 13.92/2;        // Starting z-position of SCT modules [mm]
@@ -64,10 +102,6 @@ int main(int argc, char* argv[]) {
     double length=128.05;
     int nlayers = 50;
     std::vector<std::pair<double, bool>> module_params{{0,false}, {M_PI/2,true}, {0, true}, {M_PI/2,true}};
-    // std::vector<std::pair<double, double>> module_offsets{{0.5, 0.5}, {0.5, 1.5}, {0.5, 2.5}, {0.5, 3.5},
-    //                                                       {0.5, -0.5}, {0.5, -1.5}, {0.5,-2.5}, {0.5, -3.5}, 
-    //                                                       {-0.5, 0.5}, {-0.5, 1.5}, {-0.5, 2.5}, {-0.5, 3.5},
-    //                                                       {-0.5, -0.5}, {-0.5, -1.5}, {-0.5,-2.5}, {-0.5, -3.5}};
     
     for (int i{0}; i < nlayers; i++)
     {   
@@ -130,46 +164,121 @@ int main(int argc, char* argv[]) {
 
     }
 
-    
-    //* Main event loop
-    for (unsigned int event_idx{0}; event_idx < reader.get_nentries(); event_idx++)
-    {       
-        if (event_idx > 100) break;
-
-        Event event = reader.get_event(event_idx);
-
-        std::cout << event_idx << "/" << reader.get_nentries() << ": " << event << std::endl;
-
-        // if (abs(event.nu_pdgc) != 16) continue;
-
-        // Centre hits
-        // for (auto&  hit: event.hits)
-        // {
-        //     hit.x -= event.vertex_x;
-        //     hit.y -= event.vertex_y;
-        //     // hit.z -= event.vertex_z;
-        // }
-
-        // for (auto&  hit: event.true_hits)
-        // {
-        //     hit.x -= event.vertex_x;
-        //     hit.y -= event.vertex_y;
-        //     // hit.z -= event.vertex_z;
-        // }
-
-        Event new_event = event;
-        std::vector<Hit> new_hits{};  //TODO: Write out truth and digitized hits to seperate branches
-        for (const auto& module: modules)
+    //* Digitize the hits
+    auto resultDf = df.Define("processed_hits", [&](const std::vector<double>& x, const std::vector<double>& y,
+        const std::vector<double>& z, const std::vector<double>& charge,
+        const std::vector<double>& energy, const std::vector<int>& pdgc,
+        const std::vector<int>& layer) 
         {
-            std::vector<Hit> digit_hits = module.digitizeHits(event.hits);
-            new_hits.insert(new_hits.end(), digit_hits.begin(), digit_hits.end());
-        }
+            return processOneEvent(x, y, z, charge, energy, pdgc, layer, modules);
+        }, 
+        {"x", "y", "z", "charge", "E", "pdgc", "layer"});
+    
+    
+    // resultDf.Foreach([](const std::vector<Hit>& hits) {
+    //     // std::cout << "Processed " << hits.size() << " hits.\n";
+    // }, {"processed_hits"});
 
-        new_event.hits = new_hits;
-        new_event.true_hits = event.true_hits;
-        
-        writer.write_event(new_event);
-    }
+
+    resultDf = resultDf.Define("true_x", "x");
+    resultDf = resultDf.Define("true_y", "y");
+    resultDf = resultDf.Define("true_z", "z");
+    resultDf = resultDf.Define("true_E", "E");
+    resultDf = resultDf.Define("true_pdgc", "pdgc");
+    resultDf = resultDf.Define("true_charge", "charge");
+    resultDf = resultDf.Define("true_layer", "layer");
+
+    resultDf = resultDf.Define("digit_x", [](const std::vector<Hit>& hits) {
+        std::vector<double> x;
+        for (const auto& hit : hits) {
+            x.push_back(hit.x);
+        }
+        return x;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_y", [](const std::vector<Hit>& hits) {
+        std::vector<double> y;
+        for (const auto& hit : hits) {
+            y.push_back(hit.y);
+        }
+        return y;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_z", [](const std::vector<Hit>& hits) {
+        std::vector<double> z;
+        for (const auto& hit : hits) {
+            z.push_back(hit.z);
+        }
+        return z;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_pdgc", [](const std::vector<Hit>& hits) {
+        std::vector<int> pdgc;
+        for (const auto& hit : hits) {
+            pdgc.push_back(hit.pdgc);
+        }
+        return pdgc;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_E", [](const std::vector<Hit>& hits) {
+        std::vector<double> E;
+        for (const auto& hit : hits) {
+            E.push_back(hit.energy);
+        }
+        return E;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_charge", [](const std::vector<Hit>& hits) {
+        std::vector<double> charge;
+        for (const auto& hit : hits) {
+            charge.push_back(hit.charge);
+        }
+        return charge;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_layer", [](const std::vector<Hit>& hits) {
+        std::vector<int> layer;
+        for (const auto& hit : hits) {
+            layer.push_back(hit.layer);
+        }
+        return layer;
+    }, {"processed_hits"});
+    resultDf = resultDf.Define("digit_counter", [](const std::vector<Hit>& hits) {
+        std::vector<int> layer;
+        for (const auto& hit : hits) {
+            layer.push_back(hit.counter);
+        }
+        return layer;
+    }, {"processed_hits"});
+    
+
+    resultDf.Snapshot("Hits", outputFile.c_str(), 
+    {"fEvent",       
+    "vertex_x",     
+    "vertex_y",     
+    "vertex_z",     
+    "nu_E",         
+    "nu_px",        
+    "nu_py",        
+    "nu_pz",        
+    "nu_pdgc",      
+    "target_pdgc",  
+    "isCC",         
+    "cclepton_pdgc",
+    "cclepton_E",   
+    "cclepton_px",  
+    "cclepton_py",  
+    "cclepton_pz",  
+    "digit_x",            
+    "digit_y",            
+    "digit_z",            
+    "digit_E",            
+    "digit_pdgc",         
+    "digit_charge",       
+    "digit_layer",        
+    "digit_counter",      
+    "true_x",       
+    "true_y",       
+    "true_z",       
+    "true_E",       
+    "true_pdgc",    
+    "true_charge",  
+    "true_layer"}
+    );
 
     return 0;
 }
