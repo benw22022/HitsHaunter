015538f68122a38bfed0b66b510961f07bda547c
Ben Wilson <benjamin.james.wilson@cern.ch>
2025-04-09 10:54:14 +0100

    add prints

--- python/skimmer.py
+++ python/skimmer.py
@@ -9,10 +9,12 @@ def main(args):
     filters = []
     
     for pdgc in args.has_hit:
+        print(f"Selecting events with absolute pdg code {pdgc}")
         df = df.Define(f"has_{abs(pdgc)}", f"std::find(pdgc.begin(), pdgc.end(), {abs(pdgc)}) != pdgc.end() || std::find(pdgc.begin(), pdgc.end(), -{abs(pdgc)}) != pdgc.end()")
         filters.append(f"has_{abs(pdgc)}")
     
     for pdgc in args.keep_hit:
+        print(f"Keeping hits with absolute pdg code {pdgc}")
         df = df.Define("x", f"x[abs(pdgc) == {abs(pdgc)}]")
         df = df.Define("y", f"y[abs(pdgc) == {abs(pdgc)}]")
         df = df.Define("z", f"z[abs(pdgc) == {abs(pdgc)}]")
@@ -21,22 +23,35 @@ def main(args):
         df = df.Define("charge", f"charge[abs(pdgc) == {abs(pdgc)}]")
         df = df.Define("layer", f"layer[abs(pdgc) == {abs(pdgc)}]")
     
-    cc_el_filter = "abs(nu_pdgc) == 12 && isCC == 1"
-    cc_mu_filter = "abs(nu_pdgc) == 14 && isCC == 1"
-    cc_ta_filter = "abs(nu_pdgc) == 16 && isCC == 1"
-    nc_filter = "isCC == 0"
-    
-    if "CCe" in args.interaction: filters.append(cc_el_filter)
-    if "CCmu" in args.interaction: filters.append(cc_mu_filter)
-    if "CCtau" in args.interaction: filters.append(cc_ta_filter)
-    if "NC" in args.interaction: filters.append(nc_filter)
-    
+    if args.interaction != []:
+        cc_el_filter = "abs(nu_pdgc) == 12 && isCC == 1"
+        cc_mu_filter = "abs(nu_pdgc) == 14 && isCC == 1"
+        cc_ta_filter = "abs(nu_pdgc) == 16 && isCC == 1"
+        nc_filter = "isCC == 0"
+        
+        if "CCe" in args.interaction: 
+            print("Selecting CC electron events")
+            filters.append(cc_el_filter)
+            
+        if "CCmu" in args.interaction: 
+            filters.append(cc_mu_filter)
+            print("Selecting CC muon events")
+            
+        if "CCtau" in args.interaction: 
+            filters.append(cc_ta_filter)
+            print("Selecting CC tau events")
+            
+        if "NC" in args.interaction: 
+            filters.append(nc_filter)
+            print("Selecting NC events")
+
     # Combine all filters into a single string
     combined_filter = " && ".join(filters)
     skimmed_df = df.Filter(combined_filter)
 
     # Configure number of events
     if args.nevents > 0:
+        print(f"Keeping only the first {args.nevents} events")
         skimmed_df = skimmed_df.Range(args.nevents)
     
     # Save the filtered data
